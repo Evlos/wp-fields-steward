65dbcbd61bb64d661eef32896185c158bb665773
Evlos <4tyle8@gmail.com>
2014-02-18 21:03:36 +0800

    increased version to 1.0.1 & added support for caption content

--- wp-fields-steward.php
+++ wp-fields-steward.php
@@ -1,22 +1,24 @@
 <?php
 /**
  * @package WP-Fields-Steward
- * @version 1.0.0
+ * @version 1.0.1
  */
 /*
 Plugin Name: WP-Fields-Steward
 Plugin URI: https://github.com/Evlos/WP-Fields-Steward/
 Description: Wordpress plugin works on outputing media content from custom fields in specified position.
-Version: 1.0.0
+Version: 1.0.1
 Author: Evlos
 Author URI: http://chaosoverflow.com/
 License: GPLv2 or later
 */
 
 function FS_adding_box_post($post) {
+	// Add meta box on post type post
     add_meta_box('my-meta-box', __('WP-Fields-Steward'), 'FS_render_box', 'post', 'normal', 'default');
 }
 function FS_adding_box_page($post) {
+	// Add meta box on page type post
     add_meta_box('my-meta-box', __('WP-Fields-Steward'), 'FS_render_box', 'page', 'normal', 'default');
 }
 function FS_render_box($post, $metabox) {
@@ -26,7 +28,8 @@ function FS_render_box($post, $metabox) {
 		<p><strong>Example:</strong></p>
 		<p>http://www.youtube.com/watch?v=0lPgJfvskig</p>
 		<p>http://localhost/wp-content/uploads/2014/01/14372.jpg</p>
-		<p><strong>Then run wp_fields_steward() at anywhere.</strong></p>
+		<p>http://localhost/wp-content/uploads/2014/01/14372.jpg#Caption Content</p>
+		<p><strong>Then run wp_fields_steward() anywhere in your theme.</strong></p>
 	</div>
 	<textarea name="fields_steward" id="fields_steward" placeholder="Paste here."><?php echo $data; ?></textarea>
 	<script type="text/javascript">
@@ -52,29 +55,41 @@ function FS_render_box($post, $metabox) {
 	</script>
 	<?php
 }
+// Hook them
 add_action('add_meta_boxes_post', 'FS_adding_box_post');
 add_action('add_meta_boxes_page', 'FS_adding_box_page');
 
 function FS_process_data($post) {
 	$source = $_POST['fields_steward'];
 	$data = split("\n", $source);
+	// Prepare to render input data to html code
 	$meta = '';
 	foreach ($data as $url) {
 		$url = str_replace(array("\n", "\r"), '', $url);
+		// Find out if it is a Youtube video
 		if (preg_match('/youtube\.com\/watch/i', $url)) {
 			$res = str_replace('http://www.youtube.com/watch?v=', '<iframe width="560" height="315" src="//www.youtube.com/embed/', $url);
 			$res .= '" frameborder="0" allowfullscreen></iframe>';
 		}
 		else {
-			$res = '<img src="'.$url.'" />';
+			// Find out if it contains content of caption
+			$url_split = split('#', $url);
+			if (count($url_split) == 2) {
+				$res = '<img alt="'.$url_split[1].'" src="'.$url_split[0].'" />';
+			}
+			else {
+				$res = '<img src="'.$url.'" />';
+			}
 		}
 		$meta .= '<div class="fields_steward_object">'.$res.'</div>'."\n";
 	}
+	// Save data into database
 	update_post_meta($post, 'FS_source', $source);
 	update_post_meta($post, 'FS_html', $meta);
 }
 add_action('save_post', 'FS_process_data');
 
 function wp_fields_steward($post) {
+	// Output these data from database directly
 	echo get_post_meta($post, 'FS_html', true);
 }
